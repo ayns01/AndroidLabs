e1f727bf24c2b20ef6828334703419333b3eb8bb
ayns <peenats1203@gmail.com>
2018-12-07 12:25:09 +0900

    Create Lesson2 Labs

--- Lab2_1_intents/app/src/main/java/com/example/ayana/twoactivity/MainActivity.java
+++ Lab2_1_intents/app/src/main/java/com/example/ayana/twoactivity/MainActivity.java
@@ -16,10 +16,22 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE_TWO = "com.example.ayana.twoactivity.extra.MESSAGE_TWO";
     public static final String EXTRA_MESSAGE_THREE = "com.example.ayana.twoactivity.extra.MESSAGE_THREE";
     public static final int TEXT_REQUEST = 1;
+    public static final int SHOP_REQUEST = 2;
 
     private EditText mMessageEditText;
     private TextView mReplyHeadTextView;
     private TextView mReplyTextView;
+
+    private TextView mItem1TextView;
+    private TextView mItem2TextView;
+    private TextView mItem3TextView;
+    private TextView mItem4TextView;
+    private TextView mItem5TextView;
+    private TextView mItem6TextView;
+    private TextView mItem7TextView;
+    private TextView mItem8TextView;
+    private TextView mItem9TextView;
+    private TextView mItem10TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +41,134 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText = findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
+
+        mItem1TextView = findViewById(R.id.item1);
+        mItem2TextView = findViewById(R.id.item2);
+        mItem3TextView = findViewById(R.id.item3);
+        mItem4TextView = findViewById(R.id.item4);
+        mItem5TextView = findViewById(R.id.item5);
+        mItem6TextView = findViewById(R.id.item6);
+        mItem7TextView = findViewById(R.id.item7);
+        mItem8TextView = findViewById(R.id.item8);
+        mItem9TextView = findViewById(R.id.item9);
+        mItem10TextView = findViewById(R.id.item10);
+
+        if (savedInstanceState != null) {
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+
+            boolean isItem1Visible = savedInstanceState.getBoolean("item1_visible");
+            boolean isItem2Visible = savedInstanceState.getBoolean("item2_visible");
+            boolean isItem3Visible = savedInstanceState.getBoolean("item3_visible");
+            boolean isItem4Visible = savedInstanceState.getBoolean("item4_visible");
+            boolean isItem5Visible = savedInstanceState.getBoolean("item5_visible");
+            boolean isItem6Visible = savedInstanceState.getBoolean("item6_visible");
+            boolean isItem7Visible = savedInstanceState.getBoolean("item7_visible");
+            boolean isItem8Visible = savedInstanceState.getBoolean("item8_visible");
+            boolean isItem9Visible = savedInstanceState.getBoolean("item9_visible");
+            boolean isItem10Visible = savedInstanceState.getBoolean("item10_visible");
+
+
+            if (isItem1Visible) {
+                mItem1TextView.setText(savedInstanceState.getString("selected_item1"));
+                mItem1TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem2Visible) {
+                mItem2TextView.setText(savedInstanceState.getString("selected_item2"));
+                mItem2TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem3Visible) {
+                mItem3TextView.setText(savedInstanceState.getString("selected_item3"));
+                mItem3TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem4Visible) {
+                mItem4TextView.setText(savedInstanceState.getString("selected_item4"));
+                mItem4TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem5Visible) {
+                mItem5TextView.setText(savedInstanceState.getString("selected_item5"));
+                mItem5TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem6Visible) {
+                mItem6TextView.setText(savedInstanceState.getString("selected_item6"));
+                mItem6TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem7Visible) {
+                mItem7TextView.setText(savedInstanceState.getString("selected_item7"));
+                mItem7TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem8Visible) {
+                mItem8TextView.setText(savedInstanceState.getString("selected_item8"));
+                mItem8TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem9Visible) {
+                mItem9TextView.setText(savedInstanceState.getString("selected_item9"));
+                mItem9TextView.setVisibility(View.VISIBLE);
+            }
+            if (isItem10Visible) {
+                mItem10TextView.setText(savedInstanceState.getString("selected_item10"));
+                mItem10TextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
+
+        // itemTextView
+        if (mItem1TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item1_visible", true);
+            outState.putString("selected_item1", mItem1TextView.getText().toString());
+        }
+        if (mItem2TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item2_visible", true);
+            outState.putString("selected_item2", mItem2TextView.getText().toString());
+        }
+        if (mItem3TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item3_visible", true);
+            outState.putString("selected_item3", mItem3TextView.getText().toString());
+        }
+        if (mItem4TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item4_visible", true);
+            outState.putString("selected_item4", mItem4TextView.getText().toString());
+        }
+        if (mItem5TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item5_visible", true);
+            outState.putString("selected_item5", mItem5TextView.getText().toString());
+        }
+        if (mItem6TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item6_visible", true);
+            outState.putString("selected_item6", mItem6TextView.getText().toString());
+        }
+        if (mItem7TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item7_visible", true);
+            outState.putString("selected_item7", mItem7TextView.getText().toString());
+        }
+        if (mItem8TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item8_visible", true);
+            outState.putString("selected_item8", mItem8TextView.getText().toString());
+        }
+        if (mItem9TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item9_visible", true);
+            outState.putString("selected_item9", mItem9TextView.getText().toString());
+        }
+        if (mItem10TextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("item10_visible", true);
+            outState.putString("selected_item10", mItem10TextView.getText().toString());
+        }
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
         String messge = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, messge);
@@ -51,6 +187,57 @@ public class MainActivity extends AppCompatActivity {
                 mReplyTextView.setVisibility(View.VISIBLE);
             }
         }
+
+        if (requestCode == SHOP_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String item = data.getStringExtra(SecondActivity.EXTRA_ITEM);
+                if (item != null) {
+                    switch (item) {
+                        case "Bread":
+                            mItem1TextView.setText(item);
+                            mItem1TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Meat":
+                            mItem2TextView.setText(item);
+                            mItem2TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Fish":
+                            mItem3TextView.setText(item);
+                            mItem3TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Chocolate":
+                            mItem4TextView.setText(item);
+                            mItem4TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Peach":
+                            mItem5TextView.setText(item);
+                            mItem5TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Cheese":
+                            mItem6TextView.setText(item);
+                            mItem6TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Apple":
+                            mItem7TextView.setText(item);
+                            mItem7TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Milk":
+                            mItem8TextView.setText(item);
+                            mItem8TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Lime":
+                            mItem9TextView.setText(item);
+                            mItem9TextView.setVisibility(View.VISIBLE);
+                            break;
+                        case "Candy":
+                            mItem10TextView.setText(item);
+                            mItem10TextView.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                }
+            }
+        }
+
     }
 
     public void intentFromOne(View view) {
@@ -123,5 +310,11 @@ public class MainActivity extends AppCompatActivity {
                 "fusce ut placerat.";
         intent.putExtra(EXTRA_MESSAGE_THREE, messge);
         startActivity(intent);
+    }
+
+    public void selectItemFromShoppingList(View view) {
+         Intent intent = new Intent(this, SecondActivity.class);
+         startActivityForResult(intent, SHOP_REQUEST);
+
     }
 }
--- Lab2_1_intents/app/src/main/java/com/example/ayana/twoactivity/SecondActivity.java
+++ Lab2_1_intents/app/src/main/java/com/example/ayana/twoactivity/SecondActivity.java
@@ -3,22 +3,33 @@ package com.example.ayana.twoactivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY =
-            "com.example.ayana.twoactivity.extra.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY = "com.example.ayana.twoactivity.extra.REPLY";
+    public static final String EXTRA_ITEM = "com.example.ayana.twoactivity.extra.ITEM";
     private EditText mReply;
+    TextView mTextView;
+    TextView mTextViewInScroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+
+        mTextView = findViewById(R.id.text_message);
         mReply = findViewById(R.id.editText_second);
+        mTextViewInScroll = findViewById(R.id.text_into_scroll);
 
         Intent intent = getIntent();
 
@@ -27,17 +38,14 @@ public class SecondActivity extends AppCompatActivity {
         String messageTwo = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_TWO);
         String messageThree = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_THREE);
 
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-
-        TextView textViewInScroll = findViewById(R.id.text_into_scroll);
+        mTextView.setText(message);
 
         if (messageOne != null) {
-            textViewInScroll.setText(messageOne);
+            mTextViewInScroll.setText(messageOne);
         }else if (messageTwo != null) {
-            textViewInScroll.setText(messageTwo);
+            mTextViewInScroll.setText(messageTwo);
         }else if (messageThree != null) {
-            textViewInScroll.setText(messageThree);
+            mTextViewInScroll.setText(messageThree);
         }
     }
 
@@ -46,6 +54,87 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
+        finish();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public void preserveItem(View view) {
+        String selectedItemName = null;
+        switch (view. getId()) {
+            case R.id.itemBtn1:
+                selectedItemName = "Bread";
+                break;
+
+            case R.id.itemBtn2:
+                selectedItemName = "Meat";
+                break;
+
+            case R.id.itemBtn3:
+                selectedItemName = "Fish";
+                break;
+
+            case R.id.itemBtn4:
+                selectedItemName = "Chocolate";
+                break;
+
+            case R.id.itemBtn5:
+                selectedItemName = "Peach";
+                break;
+
+            case R.id.itemBtn6:
+                selectedItemName = "Cheese";
+                break;
+
+            case R.id.itemBtn7:
+                selectedItemName = "Apple";
+                break;
+
+            case R.id.itemBtn8:
+                selectedItemName = "Milk";
+                break;
+
+            case R.id.itemBtn9:
+                selectedItemName = "Lime";
+                break;
+
+            case R.id.itemBtn10:
+                selectedItemName = "Candy";
+                break;
+        }
+
+        Intent itemIntent = new Intent();
+        itemIntent.putExtra(EXTRA_ITEM, selectedItemName);
+        setResult(RESULT_OK, itemIntent);
         finish();
     }
 }
