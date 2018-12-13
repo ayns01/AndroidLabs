287c24ecd25a87bbbedf25cf95727cfb4cd631cd
ayns <peenats1203@gmail.com>
2018-12-13 09:01:41 +0900

    Create HelloCompat

---
+++ Lab3/Lab3_3/app/src/main/java/com/example/ayana/supportlibraries/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ayana.supportlibraries;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mHelloTextView;
+
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelloTextView = findViewById(R.id.hello_textview);
+
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName,
+                "color", getApplicationContext().getPackageName());
+//        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        int colorRes;
+        if (Build.VERSION.SDK_INT >= 23) {
+            colorRes = getColor(colorResourceName);
+        }else {
+            colorRes = getResources().getColor(colorResourceName);
+        }
+        mHelloTextView.setTextColor(colorRes);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+}
